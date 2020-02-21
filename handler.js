9e1afd09f6591b07fb640732ed19180e3e617de4
ColdFerrin <ferrinkatz@ferrinkatz.com>
2020-02-21 14:29:13 -0700

    Email Sender upload

---
+++ handler.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+const SES = new AWS.SES();
+
+function sendEmail(formData, callback) {
+    const emailParams = {
+        Source: 'ferrinkatz@ferrinkatz.com', // SES SENDING EMAIL
+        ReplyToAddresses: [formData.reply_to],
+        Destination: {
+            ToAddresses: ['ferrinkatz@ferrinkatz.com'], // SES RECEIVING EMAIL
+        },
+        Message: {
+            Body: {
+                Text: {
+                    Charset: 'UTF-8',
+                    Data: `${formData.message}\n\nName: ${formData.name}\nEmail: ${formData.reply_to}`,
+                },
+            },
+            Subject: {
+                Charset: 'UTF-8',
+                Data: 'New message from ferrinkatz.com',
+            },
+        },
+    };
+
+    SES.sendEmail(emailParams, callback);
+}
+
+module.exports.staticSiteMailer = (event, context, callback) => {
+    const formData = JSON.parse(event.body);
+
+    sendEmail(formData, function(err, data) {
+        const response = {
+            statusCode: err ? 500 : 200,
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': 'https://ferrinkatz.com',
+            },
+            body: JSON.stringify({
+                message: err ? err.message : data,
+            }),
+        };
+
+        callback(null, response);
+    });
+};
